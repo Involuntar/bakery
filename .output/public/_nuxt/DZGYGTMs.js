08143cd149e3eb47801f84eeac3dd5b8f37c8fa2
Ilya <ilya.nevolin.2004@mail.ru>
2024-10-15 18:43:22 +0500

    deploy public

---
+++ .output/public/_nuxt/DZGYGTMs.js
@@ -0,0 +1,1 @@
+import{_ as n,c as o,a as r,o as t}from"./BtrBybhR.js";const a={};function s(c,e){return t(),o("div",null,e[0]||(e[0]=[r("p",null,"Main",-1)]))}const _=n(a,[["render",s]]);export{_ as default};
