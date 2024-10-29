488f01d432fe111d48ce8b2654f20d6ffb9b7a25
Sofi Katre <52908707+Sofi-Katre@users.noreply.github.com>
2024-10-29 10:36:43 +0000

    Была произведена разметка

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        <p>Main</p>
+    <div class="header">
+        <img id="IMGmainepage" src="/public/images/mainPageIMG.png" alt="mainImgPage">
+        <div class="colum1">
+            <h1>Осенняя коллекция</h1>
+            <a href="#">Открыть каталог</a>
+        </div>
+        <div class="colum2">
+            <ul>
+                <li>+7 499 322-00-20</li>
+                <li>пн-пт 10:00–20:00 (Москва)</li>
+                <li>сб-вс 10:00-17:00</li>
+            </ul>           
+        </div>
+    </div>
+    <div class="main">
+        <div class="topInfo">
+        </div>
     </div>
 </template>
+
+<style>
+
+</style>
