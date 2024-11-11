94db0e238e04a4e2eafd8c4da3bd3603a2e6e523
kuratova_ae <sofiya.katre@bk.ru>
2024-11-11 16:50:08 +0500

    Сделаны небольшие правки в стилистике, нужно будет понять как делать карточки позиций, которые на данный момент отстствуют.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="header">
-        <img id="IMGmainepage" src="/public/images/mainPageIMG.png" alt="mainImgPage">
-        <div>
+    <div class="header">      
+         
+    </div>
+    <div class="main">
+        <div class="container-info">
             <div class="colum1">
                 <h1>Осенняя коллекция</h1>
-                <a href="#">Открыть каталог</a>
+                <a id="colortext" href="">Открыть каталог</a>
             </div> 
             <div class="colum2">
                 <ul>
@@ -14,24 +16,86 @@
                 </ul>           
             </div>
         </div>
-        
-    </div>
-    <div class="main">
         <div class="topInfo">
-            <h2>В основе Biskvitnyy Dvor лежит философия семьи , домашнего тепла и уюта, направленное на заботу о вас.</h2>
-            <p>Сбалансированные сладости для встречи за кофе с подругой, уютных посиделок с близкими или свидания с собой.</p>
-            <a href="#">Открыть каталог</a>
+            <p id="H1">В основе Biskvitnyy Dvor лежит философия семьи , домашнего тепла и уюта, направленное на заботу о вас.</p>
+            <div class="blotopinfo">
+                <p>Сбалансированные сладости для встречи за кофе с подругой, уютных посиделок с близкими или свидания с собой.</p>
+                <a href="#">Открыть каталог</a>
+            </div>
         </div>
         <div class="category">
         </div>
         <div class="storylife">
-            <img src="/public/images/lifephoto.png" alt="lifePhoto">
-            <h3>Когда-то с сестрой мы играли в песочнице и мечтали печь много вкусных блюд для наших родных, сейчас мы обе выросли и воплотили свою мечту в жизнь. Мы отрыли свою собственную пекарню, чтобы люди не просто если, но еще и дарили наши сладости на праздники или просто для того. чтобы сделать своим близким приятное.</h3>
-            <h4>Всегда с любовью, ваши кондитеры Любовь и Надежда.</h4>
+            <div class="photolife">
+                <img src="/public/images/lifephoto.png" alt="lifePhoto">
+            </div>
+            <div class="storyblok">
+                <p>Когда-то с сестрой мы играли в песочнице и мечтали печь много вкусных блюд для наших родных, сейчас мы обе выросли и воплотили свою мечту в жизнь. Мы отрыли свою собственную пекарню, чтобы люди не просто если, но еще и дарили наши сладости на праздники или просто для того. чтобы сделать своим близким приятное.</p>
+                <p>Всегда с любовью, ваши кондитеры Любовь и Надежда.</p>
+            </div>
         </div>
     </div>
 </template>
 
 <style>
+ul{
+    list-style: none;
+    color: aliceblue;
+}
+.container-info{
+    color: aliceblue;
+    background-image: url("/public/images/mainPageIMG.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    padding: 30vh 7vh 2vh 7vh;
+    display: grid;
+    grid-template-columns: auto auto;
+    text-align: left;
+    gap: 10vh;
+}
 
+.colum1{
+    color: aliceblue;
+}
+.colum2{
+    color: aliceblue;
+}
+.topInfo{
+    padding: 2vh 24vh 2vh 24vh;
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 20vh;
+}
+.topInfo #H1{
+    font-family: JejuMyeongjo;
+    font-size: 32px;
+}
+.topInfo.blotopinfo p{
+    font-size: 14px;
+} 
+.storylife{
+    max-width: 1000px;
+    max-height: 580px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: auto auto;
+}
+.photolife{
+    position: relative;
+}
+.storyblok{
+    margin-top: 15vh;
+    margin-left: 30vh;
+    max-width: 600px;
+	-webkit-transform: skew(-20deg);
+	-moz-transform: skew(-20deg);
+	-o-transform: skew(-20deg);
+    font-family: JejuMyeongjo;
+    font-size: 16px;
+    color: aliceblue;
+    position: absolute;
+    padding: 8vh 10vh 8vh 10vh;
+    background-color: rgba(81, 81, 81, 0.8);
+}
 </style>
